acd590f2778ceaf8f40d3a58580ba145891f317a
Dragonek <70590182+dragoneknp@users.noreply.github.com>
2020-12-04 23:36:52 +0300

    Create Filewithstructure.h

---
+++ Programming 1sem/3 block/Filewithstructure.h
@@ -0,0 +1,15 @@
+#ifndef UNTITLED1_filewithstructure_H
+#define UNTITLED1_filewithstructure_H
+struct triangle {
+    float x1, y1;
+    float x2, y2;
+    float x3, y3;
+} tr;
+
+void scancoordinate();
+
+float counts(float, float, float, float, float, float);
+
+float countp(float, float, float, float, float, float);
+
+#endif //UNTITLED1_filewithstructure_H
