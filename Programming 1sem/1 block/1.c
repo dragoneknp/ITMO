0fc70067dc9ab1c321e392615d6464fe88a696a7
Dragonek <70590182+dragoneknp@users.noreply.github.com>
2020-12-04 23:10:24 +0300

    Create 1.c

---
+++ Programming 1sem/1 block/1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    float a;
+    char ch;
+    scanf("%f\n",&a);
+    printf("a=%f\n",a);
+    ch=getchar();
+    printf("ch=");
+    putchar(ch);
+    printf("\n");
+    return 0;
+}
