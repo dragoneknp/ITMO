d74dde12c5bd169b86f3651c515ce982a5f961dd
Dragonek <70590182+dragoneknp@users.noreply.github.com>
2020-12-04 23:33:50 +0300

    Create 11.c

---
+++ Programming 1sem/3 block/11.c
@@ -0,0 +1,10 @@
+Main.c
+#include "filewithstucture1.h"
+#include <stdio.h>
+main(){
+ float s,p;
+ scancoordinate();
+ s=counts(tr.x1,tr.y1,tr.x2,tr.y2,tr.x3,tr.y3);
+ p=countp(tr.x1,tr.y1,tr.x2,tr.y2,tr.x3,tr.y3);
+ printf("%f %f",p,s);
+}
