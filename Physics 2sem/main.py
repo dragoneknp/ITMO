0153555ff4712982df4fef54bd0261102a16056e
Dragonek <70590182+dragoneknp@users.noreply.github.com>
2021-05-24 21:10:26 +0300

    Update main.py

--- Physics 2sem/main.py
+++ Physics 2sem/main.py
@@ -1,13 +1,13 @@
 import math
 import matplotlib.pyplot as plt
-
+ 
 teta, Mr, Mt, u, alpha = map(int, input().split())
 Mearth = 5.972e+24
 teta = 90 - teta
 # 80 30 40 250 20
-R = 6371 * 10  3
+R = 6371 * 10 ** 3
 g0 = 6.67e-11 * Mearth / R / R
-
+ 
 x, y = 0, 1
 v = 0
 i = 0
@@ -18,17 +18,17 @@ Vy = 0
 plot_m, plot_v, plot_x, plot_y = [], [], [], []
 plot_Vx, plot_Vy = [], []
 i = 0
-
+ 
 while y >= 0:
     g = g0 * R * R / ((R + y) * (R + y))
-
+ 
     if m - Mr > 0:
         Vx += math.sin(teta * math.pi / 180) * u * alpha * h / (m - alpha * h)
         Vy += math.cos(teta * math.pi / 180) * u * alpha * h / (m - alpha * h)
         m -= alpha * h
     Vy -= g * h
     y -= g * h
-
+ 
     if Vx != 0:
         teta = math.atan2(Vy, -Vx) * 180 / math.pi - 90
         print(teta)
@@ -37,40 +37,42 @@ while y >= 0:
             teta = 0
         else:
             teta = 180
-
+ 
     x += Vx * h
     y += Vy * h
-
+ 
     plot_m.append(m - Mr)
-    plot_v.append(math.sqrt(Vx  2 + Vy ** 2))
+    plot_v.append(math.sqrt(Vx ** 2 + Vy ** 2))
     plot_x.append(x)
     plot_y.append(y)
     plot_Vx.append(Vx)
-    plotVy.append(Vy)
+    plot_Vy.append(Vy)
     i += 1
-
-tg = [ for _ in range(i)]
+ 
+tg = [_ for _ in range(i)]
+ 
 
 plt.plot(tg, plot_m)
 plt.xlabel("t")
 plt.ylabel("m(t)")
 plt.show()
-
+ 
 plt.plot(plot_x, plot_y)
 plt.xlabel("x")
 plt.ylabel("y")
 plt.show()
+ 
 
 plt.plot(tg, plot_v)
 plt.xlabel("t")
 plt.ylabel("V(t)")
 plt.show()
-
+ 
 plt.plot(tg, plot_Vy)
 plt.xlabel("t")
 plt.ylabel("Vy")
 plt.show()
-
+ 
 plt.plot(tg, plot_Vx)
 plt.xlabel("t")
 plt.ylabel("Vx")
